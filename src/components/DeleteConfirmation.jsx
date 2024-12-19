3a2d6b2c76bf7d214909205529349a0ac85ea8fe
Panwar, Himanshu <himanshu.panwar@boeing.com>
2024-12-19 09:16:18 +0530

    complete useEffect and useCallback use in react

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,17 +1,36 @@
-// eslint-disable-next-line react/prop-types
+/* eslint-disable react/prop-types */
+import { useEffect } from 'react';
+
+import ProgressBar from './ProgressBar.jsx';
+
+const TIMER = 300;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-    return (
-      <div id="delete-confirmation">
-        <h2>Are you sure?</h2>
-        <p>Do you really want to remove this place?</p>
-        <div id="confirmation-actions">
-          <button onClick={onCancel} className="button-text">
-            No
-          </button>
-          <button onClick={onConfirm} className="button">
-            Yes
-          </button>
-        </div>
+  useEffect(() => {
+    console.log('TIMER SET');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      console.log('Cleaning up timer');
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
+  return (
+    <div id="delete-confirmation">
+      <h2>Are you sure?</h2>
+      <p>Do you really want to remove this place?</p>
+      <div id="confirmation-actions">
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
+        <button onClick={onConfirm} className="button">
+          Yes
+        </button>
       </div>
-    );
-  }
+      <ProgressBar timer={TIMER} />
+    </div>
+  );
+}
